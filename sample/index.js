7a5f75854365695d10543a1f241be2b96dc4baa0
fedeghe <fedeghe@gmail.com>
2020-08-27 22:46:59 +0200

    basic polltergeist playgound

--- sample/index.js
+++ sample/index.js
@@ -1,3 +1,7 @@
 console.log("༺ polltergeist 0.0.0 ༻");
-console.log('powered by        ᚗᚌ');
+console.log('powered by ᚗᚌ');
+
 console.log(new Date);
+
+
+//
--- source/sample/index.js
+++ source/sample/index.js
@@ -1,3 +1,7 @@
 console.log("༺ polltergeist maltaV('PACKAGE.version') ༻");
-console.log('powered by        ᚗᚌ');
+console.log('powered by ᚗᚌ');
+
 console.log(new Date);
+
+
+//
