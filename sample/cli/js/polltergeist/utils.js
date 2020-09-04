c2b425b0b0f1fd6bd02de245088312969db37a40
fedeghe <fedeghe@gmail.com>
2020-09-04 14:52:57 +0200

    remove sample folder.. it's generated

--- sample/cli/js/polltergeist/utils.js
+++
@@ -1,9 +0,0 @@
-function encode(d) {
-    return JSON.stringify(d)
-}
-function decode(d) {
-    return d ? JSON.parse(d) : {}
-}
-function decodeData(d) {
-    return d && 'data' in d ? JSON.parse(d.data) : {}
-}
